09bfa2881c9bed6f42229f5009d7c821ffb97b9d
sebastian <sebastian.miketta@outlook.de>
2020-12-02 17:06:01 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.io.OutputStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("test");
+    }
+}
